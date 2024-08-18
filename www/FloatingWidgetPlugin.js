20d0df6b42918a574a9ad42e6d883c5a64ef186b
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-08-18 18:13:34 +0100

    Update FloatingWidgetPlugin.js

--- www/FloatingWidgetPlugin.js
+++ www/FloatingWidgetPlugin.js
@@ -3,7 +3,12 @@ var exec = require('cordova/exec');
 var FloatingWidgetPlugin = {
     showFloatingWidget: function(successCallback, errorCallback) {
         exec(successCallback, errorCallback, "FloatingWidgetPlugin", "showFloatingWidget", []);
+    },
+
+    isWidgetDisplayed: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "FloatingWidgetPlugin", "isWidgetDisplayed", []);
     }
 };
 
 module.exports = FloatingWidgetPlugin;
+
